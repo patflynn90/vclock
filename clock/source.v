8caf188f7ab21aba1e1d59673b4fdcb6039aa527
Patrick Flynn <patflynn90@icloud.com>
2025-05-02 11:15:03 -0400

    feat(color) add --color flag and pass to Clock.run
    - Introduce flag parser in main.v and migrate --center parsing
    - Introduce --color option in flag parser
    - Change Clock.run signature to (center bool, color string)
    - Pass user's color choice to Clock.run

--- clock/source.v
+++ clock/source.v
@@ -38,7 +38,7 @@ pub fn (mut c Clock) tick() []string {
 	return ascii.render_string(timestr)
 }
 
-pub fn (mut c Clock) run(center bool) {
+pub fn (mut c Clock) run(center bool, color string) {
 	for {
 		util.clear_screen()
 		util.move_cursor_home()
